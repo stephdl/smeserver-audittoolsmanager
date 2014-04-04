aeb8ebb0da8d74576c1a05e661cc19dd15753ba7
Stephane de Labrusse <stephdl@de-labrusse.fr>
2014-04-04 23:24:01 +0200

    working on the tree architecture

--- root/usr/lib/perl5/site_perl/esmith/FormMagick/Panel/audittools.pm
+++ root/usr/lib/perl5/site_perl/esmith/FormMagick/Panel/audittools.pm
@@ -7,16 +7,19 @@ use warnings;
 use esmith::FormMagick;
 use esmith::cgi;
 use esmith::util;
+use CGI::Carp qw(fatalsToBrowser);
+
+
 #use File::Basename;
 #use Exporter;
 #use Carp qw(verbose);
 #use Getopt::Long;
 
 our @ISA = qw(esmith::FormMagick Exporter);
-our @EXPORT = qw();
+our @EXPORT = qw(print_audittools);
  
 sub print_audittools {
-
+#my $param= shift;
     	my $self = shift;
     	my $q = $self->{cgi};
 
@@ -24,9 +27,15 @@ print $q->table ({border => 0, cellspacing => 0, cellpadding => 4},
     esmith::cgi::genTextRow ($q,
      $q->p ('Verify about groups and users :')));
     print $q->Tr (esmith::cgi::genButtonRow ($q,$q->submit (-name => 'state',-value => 'Verify Users and Groups')));     
-if ($q->param ('state') eq "Verify Users and Groups")
-        #{Scan_Local_Network ($q);}
-        { foreach  (`/sbin/e-smith/audittools/groups-users`)
-        print $q->br($_);}
+
+#if (  $q->param ('state') eq "Verify Users and Groups")
+       #{Scan_Local_Network ($q);}
+        #{ foreach  (`/sbin/e-smith/audittools/groups-users`)
+        #print $q->br($_);}
+{my @a=`/sbin/e-smith/audittools/groups-users;/sbin/e-smith/audittools/newrpms`;
+
+#my @a=`/sbin/e-smith/audittools/newrpms`;
+ foreach my $b (@a)
+{print $q->p($q->p ($b));}}
 }
 1;
